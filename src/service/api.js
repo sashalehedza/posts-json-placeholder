4c3a3c2c8e1059f520c5266aaddeeb485c12781a
Sasha Lehedza <sashalehedza@gmail.com>
2021-12-11 11:13:32 +0200

    pointless comments deleted

--- src/service/api.js
+++ src/service/api.js
@@ -1,18 +1,7 @@
 import axios from 'axios'
 
-// const postsUrl =
-//   'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=2&api_key=DEMO_KEY'
 const postsUrl = 'https://jsonplaceholder.typicode.com/posts'
-// export const getPosts = async (userId) => {
-//   if (!userId) {
-//     return await axios.get(`${postsUrl}`)
-//   } else if (userId) {
-//     return await axios.get(`${postsUrl}?userId=${userId}`)
-//   }
-// }
 
 export const getPosts = async () => {
   return await axios.get(`${postsUrl}`)
 }
-
-// https://jsonplaceholder.typicode.com/posts?userId=1
